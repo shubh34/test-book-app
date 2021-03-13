edb1497fdb320ca9c45126fd7cc77d5757f191f0
Shubham Jan <shubham.jain@rakuten.com>
2021-03-13 10:43:40 +0100

    App routes

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,13 +3,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+
+export const history = createBrowserHistory();
 
 const store = (initialState = {}) => {
   const middleWares =
     process.env.NODE_ENV === 'production'
-      ? applyMiddleware(apiMiddleware, thunk)
-      : composeWithDevTools(applyMiddleware(apiMiddleware, thunk));
-  return createStore(reducers, initialState, middleWares);
+      ? applyMiddleware(routerMiddleware(history), apiMiddleware, thunk)
+      : composeWithDevTools(applyMiddleware(routerMiddleware(history), apiMiddleware, thunk));
+  return createStore(reducers(history), initialState, middleWares);
 };
 
 export default store;
--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import books from '../../states/books/reducer';
 import applicationConfig from '../../states/applicationConfig/reducer';
+import { connectRouter } from 'connected-react-router';
 
-export default combineReducers({
-  books,
-  applicationConfig
-});
+export const reducers = (history: Object) =>
+  combineReducers({
+    router: connectRouter(history),
+    books,
+    applicationConfig
+  });
+export default reducers;
